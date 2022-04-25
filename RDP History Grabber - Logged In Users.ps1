e7165a7bdc971b69b6326eaaf13acf59c23bd918
Andrew Metallinos <andrew@metallinostech.com.au>
2022-04-25 14:48:13 +1000

    Update and rename ListLogged-inUsers.ps1 to RDP History Grabber - Logged In Users.ps1

---
+++ RDP History Grabber - Logged In Users.ps1
@@ -0,0 +1,77 @@
+ <#
+    .SYNOPSIS
+
+        This function will generate a list
+        of all past RDP connections made
+        on this PC for all logged in users.
+
+
+    .INFORMATION
+
+        Original Source: https://github.com/3gstudent/List-RDP-Connections-History
+        Author: 3gstudent@3gstudent
+        License: BSD 3-Clause
+#>
+
+
+
+Write-Host "
+
+    ███╗░░░███╗███████╗████████╗░█████╗░██╗░░░░░██╗░░░░░██╗███╗░░██╗░█████╗░░██████╗  ████████╗███████╗░█████╗░██╗░░██╗
+    ████╗░████║██╔════╝╚══██╔══╝██╔══██╗██║░░░░░██║░░░░░██║████╗░██║██╔══██╗██╔════╝  ╚══██╔══╝██╔════╝██╔══██╗██║░░██║
+    ██╔████╔██║█████╗░░░░░██║░░░███████║██║░░░░░██║░░░░░██║██╔██╗██║██║░░██║╚█████╗░  ░░░██║░░░█████╗░░██║░░╚═╝███████║
+    ██║╚██╔╝██║██╔══╝░░░░░██║░░░██╔══██║██║░░░░░██║░░░░░██║██║╚████║██║░░██║░╚═══██╗  ░░░██║░░░██╔══╝░░██║░░██╗██╔══██║
+    ██║░╚═╝░██║███████╗░░░██║░░░██║░░██║███████╗███████╗██║██║░╚███║╚█████╔╝██████╔╝  ░░░██║░░░███████╗╚█████╔╝██║░░██║
+    ╚═╝░░░░░╚═╝╚══════╝░░░╚═╝░░░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝╚═╝░░╚══╝░╚════╝░╚═════╝░  ░░░╚═╝░░░╚══════╝░╚════╝░╚═╝░░╚═╝
+
+
+    Title: RDP History Grabber
+    Author: Andrew Metallinos <andrew@metallinostech.com.au>
+    Creation Date: 24/04/2022
+    Revision Date: 25/04/2022
+    Version: 1.0.0
+
+========================================
+"
+
+
+
+$AllUser = Get-WmiObject -Class Win32_UserAccount
+foreach($User in $AllUser)
+{
+	$RegPath = "Registry::HKEY_USERS\"+$User.SID+"\Software\Microsoft\Terminal Server Client\Servers\"
+	Write-Host "User:"$User.Name
+	Write-Host "SID:"$User.SID
+	Write-Host "Status:"$User.Status
+	Try  
+    	{ 
+		$QueryPath = dir $RegPath -Name -ErrorAction Stop
+	}
+	Catch
+	{
+		Write-Host "No RDP Connections History"
+		Write-Host "
+----------------------------------------
+"
+		continue
+	}
+	foreach($Name in $QueryPath)
+	{   
+		Try  
+    		{  
+    			$User = (Get-ItemProperty -Path $RegPath$Name -ErrorAction Stop).UsernameHint
+    			Write-Host "`nServer:"$Name
+    			Write-Host "User:"$User
+    		}
+    		Catch  
+    		{
+			Write-Host "No RDP Connections History"
+    		}
+	}
+	Write-Host "
+----------------------------------------
+"	
+}
+
+Read-Host -Prompt "
+Press ENTER to close this window"
